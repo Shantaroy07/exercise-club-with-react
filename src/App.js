5b88231e3c3a9c18944a82a1a0c15389164af090
Shantaroy07 <shantaroy148@gmail.com>
2022-09-28 20:39:10 +0600

    'first-commit'

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './Components/Header/Header';
-import Shop from './Components/Shop/Shop';
+import { useState } from 'react';
 
 
 function App() {
   return (
-    <div >
-      <Header></Header>
-      <Shop></Shop>
+    <div className='App'>
+
     </div>
   )
 }
-
 export default App;
